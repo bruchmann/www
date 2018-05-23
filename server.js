cbfe31276d29cb138b2dac083016498475575eb6
Steffen Bruchmann <hi@sbruchmann.me>
2018-05-23 22:43:34 +0200

    Use environment variables for port and host
    Signed-off-by: Steffen Bruchmann <hi@sbruchmann.me>

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const path = require("path");
 
 const express = require("express");
 
+const port = process.env.PORT || 5000;
+const host = process.env.HOST || "localhost";
+
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 const server = http.createServer(app);
-server.listen(5000);
+server.listen(port, host);
